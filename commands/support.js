85ad7dd9f43233d678ebb6cdff1664dbd78e5028
l7aj⛧ <164557562+l7qj@users.noreply.github.com>
2024-06-01 21:40:16 +0100

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -30,14 +30,14 @@ module.exports = {
     try {
 
       const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-      const githubLink = "https://github.com/GlaceYT";
-      const replitLink = "https://replit.com/@GlaceYT";
-      const youtubeLink = "https://www.youtube.com/@GlaceYT";
+      const githubLink = "https://github.com";
+      const replitLink = "https://replit.com";
+      const youtubeLink = "https://www.youtube.com";
         const embed = new EmbedBuilder()
             .setColor('#b300ff')
             .setAuthor({
               name: 'Support Server',
-              iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
+              iconURL: 'https://images-ext-1.discordapp.net/external/gBqnIlSJ9EbT70umwH3M6bBgn3JKwR1ve5WPWZiCUR0/https/media.tenor.com/rZm2DTx3FKIAAAPo/monkey-music-monkey.mp4', 
               url: 'https://discord.gg/xQF9f9yUEM'
           })
             .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
