fa27ccbcf069e9617ef8c3380344e513c2f5ee6d
l7aj⛧ <164557562+l7qj@users.noreply.github.com>
2024-06-01 21:48:05 +0100

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -29,7 +29,7 @@ module.exports = {
   run: async (client, interaction) => {
     try {
 
-      const supportServerLink = "https://discord.gg/xQF9f9yUEM";
+      const supportServerLink = "https://discord.gg/pDF8G5hB";
       const githubLink = "https://github.com";
       const replitLink = "https://replit.com";
       const youtubeLink = "https://www.youtube.com";
@@ -37,11 +37,11 @@ module.exports = {
             .setColor('#b300ff')
             .setAuthor({
               name: 'Support Server',
-              iconURL: 'https://images-ext-1.discordapp.net/external/gBqnIlSJ9EbT70umwH3M6bBgn3JKwR1ve5WPWZiCUR0/https/media.tenor.com/rZm2DTx3FKIAAAPo/monkey-music-monkey.mp4', 
-              url: 'https://discord.gg/xQF9f9yUEM'
+              iconURL: 'https://tenor.com/view/monkey-music-monkey-monkey-listening-to-the-music-monkey-chill-vibin-gif-24320967', 
+              url: 'https://discord.gg/pDF8G5hB'
           })
             .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
-            .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
+            .setImage('https://tenor.com/view/monkey-music-monkey-monkey-listening-to-the-music-monkey-chill-vibin-gif-24320967')
             .setTimestamp();
 
       return interaction.reply({ embeds: [embed] });
