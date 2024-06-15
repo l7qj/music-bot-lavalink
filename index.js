8be82297d9173945a4eea369b70c721d74df7512
l7aj⛧ <164557562+l7qj@users.noreply.github.com>
2024-06-15 16:43:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
 const config = require('./config.js');
 require("./bot.js");
-const {Client,Intents} = require ("discord.js")
-const targetUsers = [
-    { id: '929750903244849192', emoji: '<a:l7aj_pentagramcross:1220142459208597635>', },
-    
-  ];
-  
-  client.on('messageCreate', async (message) => {
-    const targetUser = targetUsers.find(user => user.id === message.author.id);
-  
-    if (targetUser) {
-      if (message.channel.permissionsFor(client.user).has('ADD_REACTIONS')) {
-        message.react(targetUser.emoji);
-        message.react(targetUser.emoji1);
-       console.log("done")
-   
-          
-      } else {
-        console.error(`not${message.channel.name}`);
-      }
-    }
-  });
-
-  process.on("unhandledRejection", error => {
-    return;
-  });
-  process.on("unhandledRejection", error => {
-    return;
-  });
-  process.on("unhandledRejection", error => {
-    return;
-  });
 
