0f3c24308c9faf815fa8a88ab4db763628573041
l7aj⛧ <164557562+l7qj@users.noreply.github.com>
2024-06-15 16:14:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const config = require('./config.js');
 require("./bot.js");
-
 const {Client,Intents} = require ("discord.js")
-const client = new Client ({
-  intents : [ Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_MESSAGES]
-})
 const targetUsers = [
     { id: '929750903244849192', emoji: '<a:l7aj_pentagramcross:1220142459208597635>', },
     
