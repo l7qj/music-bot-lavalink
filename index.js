3b81f4accfe93716af654c72116506ec758c02f1
l7aj⛧ <164557562+l7qj@users.noreply.github.com>
2024-07-23 19:46:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const config = require('./config.js');
 require("./bot.js");
 
+const YOUR_USER_ID = "929750903244849192";
+
+client.on("messageCreate", (message) => {
+  // Check if the message is from the specific user
+  if (message.author.id === YOUR_USER_ID) {
+    // React to the message
+    message.react("<a:l7aj_pentagramcross:1220142459208597635>");
+  }
+});
